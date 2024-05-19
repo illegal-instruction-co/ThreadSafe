b8a4a60f3f63c01de4e84a70f910f0a51a19381b
illegal-instruction-co <machinetherapist2@gmail.com>
2024-05-19 13:01:42 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# Ring3 Thread Safety Checks for Windows
+
+This code handles thread safety checks in primative level. 
+
+## Purpose of the Code
+The code aims to create a thread that continuously monitors its own safety by checking specific execution metrics. This is achieved by hooking into the SleepEx function from the Windows API to validate the state of the thread periodically. The primary objective is to detect anomalies such as thread termination or manipulation, which could indicate potential security threats or instability.
+
+## Key Features
+### Thread Creation and Monitoring:
+
+The SafeThread class inherits from std::thread and initializes a new thread.
+Upon initialization, the thread ID is stored, and hooks are set up using MinHook to intercept the SleepEx function.
+### Hooked SleepEx Function:
+
+The SleepExHook function is a custom implementation that replaces the original SleepEx function.
+It checks if the thread is still running by verifying the thread's exit code and its state.
+### RDTSC Checks:
+
+The CheckRDTSC and FetchRDTSC methods are used to monitor the processor's timestamp counter (RDTSC).
+These checks ensure that the thread's execution timing remains within expected bounds, helping to detect any unusual delays or interruptions.
+### Reaction to Anomalies:
+
+If an anomaly is detected (e.g., the thread is not found or terminated), the React method is triggered.
+This method shows an error message and intentionally crashes the program to prevent further execution, indicating a critical issue with thread safety.
+### Example Usage
+In the main function, a SafeThread instance is created with a lambda function that includes periodic safety checks. The thread runs indefinitely, printing a "Thread is safe!" message every second. If any safety check fails, appropriate measures are taken to handle the anomaly.
+
+This code is useful in scenarios where thread integrity is crucial, such as in security-sensitive applications or systems requiring high reliability and tamper detection.
---
+++ ThreadSafe.cpp
@@ -0,0 +1,91 @@
+﻿#include "minhook/include/MinHook.h"
+
+#include <iostream>
+#include <thread>
+#include <functional>
+#include <string>
+#include <chrono>
+#include <vector>
+
+#include <windows.h>
+
+class SafeThread final : public std::thread {
+public:
+	typedef LRESULT(WINAPI* SleepExType)(DWORD dwMilliseconds, BOOL bAlertable);
+	static inline SleepExType SleepExOriginal = nullptr;
+
+	SafeThread(const std::function<void(const std::function<void()>&)>& func) : std::thread([this, func]() {
+		_threadIds.push_back(GetCurrentThreadId());
+
+		MH_Initialize();
+		MH_CreateHookApi(L"kernelbase.dll", "SleepEx", &SleepExHook, reinterpret_cast<LPVOID*>(&SleepExOriginal));
+		MH_EnableHook(MH_ALL_HOOKS);
+
+		func([this]() {CheckRDTSC(); FetchRDTSC(); });
+
+	}) {}
+
+	static LRESULT SleepExHook(DWORD dwMilliseconds, BOOL bAlertable) {
+		for (const auto& threadId : _threadIds) {
+			HANDLE handle = OpenThread(THREAD_ALL_ACCESS, FALSE, threadId);
+
+			if (!handle)
+				return React("Thread is not found!");
+
+			DWORD exitCode = 0;
+			GetExitCodeThread(handle, &exitCode);
+			CloseHandle(handle);
+
+			if (exitCode != 259)
+				return React("Thread is terminated!");
+		}
+
+		return SleepExOriginal(dwMilliseconds, bAlertable);
+	}
+private:
+	static constexpr __int64 _maxDiff = 5000000000;
+	__int64 _rdtsc = 0;
+	static inline std::vector<DWORD> _threadIds;
+
+	__forceinline void FetchRDTSC() {
+		_rdtsc = __rdtsc();
+	}
+
+	__forceinline void CheckRDTSC() {
+		std::cout << "Current: " << __rdtsc() << " | Previous: " << _rdtsc << std::endl;
+
+		if (_rdtsc && __rdtsc() > _rdtsc + _maxDiff)
+			React("Thread is not safe!");
+	}
+
+	static __forceinline DWORD React(const std::string& message) {
+		std::thread([]() {
+				std::this_thread::sleep_for(std::chrono::milliseconds(500));
+
+				for (long long int i = 0; ++i; (&i)[i] = i)
+					*((unsigned int*)0) = 0xDEADBEEF;
+
+		}).detach();
+
+		MessageBoxA(nullptr, message.c_str(), "Error", MB_OK | MB_ICONERROR);
+
+		return 0xFEEDBABE;
+	}
+};
+
+int main() {
+
+	SafeThread thread([](const std::function<void()>& checks) {
+		for (;; std::this_thread::sleep_for(std::chrono::seconds(1))) {
+			checks();
+
+			std::cout << "Thread is safe!" << std::endl;
+		}
+	});
+
+	thread.join();
+
+	for(;; std::this_thread::sleep_for(std::chrono::seconds(1)));
+
+	return getchar();
+}
